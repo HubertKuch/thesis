5546637b4dadb1dcb4b2988623841bd5abcd11de
Hubert Kuch <kuchhubert@gmail.com>
2025-07-24 23:30:26 +0200

    feat: template method on account update request

--- src/main/java/pl/hubertkuch/thesis/account/command/AccountUpdateRequest.java
+++ src/main/java/pl/hubertkuch/thesis/account/command/AccountUpdateRequest.java
@@ -3,4 +3,7 @@ package pl.hubertkuch.thesis.account.command;
 public record AccountUpdateRequest(
         String slug
 ) {
+    public static AccountUpdateRequest fromSlug(String slug) {
+        return new AccountUpdateRequest(slug);
+    }
 }
