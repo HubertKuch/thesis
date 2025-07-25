b926311180a2ce9ffbecd2650d04a2a4183b7a4d
Hubert Kuch <kuchhubert@gmail.com>
2025-07-25 19:53:40 +0200

    feat: add `findByNameAndOwner` method to repo

--- src/main/java/pl/hubertkuch/thesis/application/repo/ApplicationRepo.java
+++ src/main/java/pl/hubertkuch/thesis/application/repo/ApplicationRepo.java
@@ -2,8 +2,12 @@ package pl.hubertkuch.thesis.application.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.hubertkuch.thesis.account.Account;
 import pl.hubertkuch.thesis.application.Application;
+
+import java.util.Optional;
 
 @Repository
 public interface ApplicationRepo extends JpaRepository<Application, String> {
+    Optional<Application> findByNameAndOwner(String name, Account owner);
 }
