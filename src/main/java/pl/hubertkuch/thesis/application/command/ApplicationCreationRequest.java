85c3541e5fd8d4872b13a91248bd282f9cc40597
Hubert Kuch <kuchhubert@gmail.com>
2025-07-25 19:53:53 +0200

    feat: validation for app cmd creation

--- src/main/java/pl/hubertkuch/thesis/application/command/ApplicationCreationRequest.java
+++ src/main/java/pl/hubertkuch/thesis/application/command/ApplicationCreationRequest.java
@@ -1,6 +1,16 @@
 package pl.hubertkuch.thesis.application.command;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import pl.hubertkuch.thesis.account.Account;
 
-public record ApplicationCreationRequest(String name, String description, Account owner) {
+public record ApplicationCreationRequest(
+        @Min(2) @Max(8)
+        String name,
+        @Min(2) @Max(8)
+        String description,
+        @NotNull
+        Account owner
+) {
 }
